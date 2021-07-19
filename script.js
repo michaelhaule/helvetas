738343176028f0b234c6d520dc894d81b6216d97
Mikael Emmanuel <mikael.haule1@gmail.com>
2021-07-19 22:41:38 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-  axios.get('https://belltro.xyz:129/numbers')
-  .then(function(response){
-    let users = response.data.data;
-    // console.log(users)
-    let output = '';
-    users.forEach(function(user) {
-      output += `
+axios.get("https://belltro.xyz:129/numbers").then(function (response) {
+  let users = response.data.data;
+//   console.log(users)
+  let output = "";
+  users.forEach(function (user) {
+    output += `
       <li>
-      <a href="#">
+      <a href="#" onclick="return myFunction();">
           <div class="d-flex">                            
               <div class="chat-user-img online align-self-center me-3 ms-0">
                   <img src="assets/images/users/avatar-3.jpg" class="rounded-circle avatar-xs" alt="">
-                  <span class="user-status"></span>
               </div>
 
               <div class="flex-1 overflow-hidden">
-                  <h5 class="text-truncate font-size-15 mb-1">${user.replace(/whatsapp:/g,'')}</h5>
-                  <p class="chat-user-message text-truncate mb-0">Hey! there I'm available</p>
+                  <h5 class="text-truncate font-size-15" style="margin-top: 0.50rem;">${user.replace(
+                    /whatsapp:/g,
+                    ""
+                  )}</h5>
               </div>
-              <div class="font-size-11">05 min</div>
           </div>
       </a>
-  </li>`;
-    });
-    document.getElementById('output').innerHTML = output;
-    // console.log(response.status); // ex.: 200
+  </li>
+  `;
+
   });
+  document.getElementById("output").innerHTML = output;
+  // console.log(response.status); // ex.: 200
+});
 
 
 
