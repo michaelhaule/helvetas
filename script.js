3e8c90d4473a515166c5466611f8307d9e1fb31b
Mikael Emmanuel <mikael.haule1@gmail.com>
2021-07-24 14:38:16 +0300

    Adding to script and DP

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 axios.get("https://belltro.xyz:129/numbers").then(function (response) {
   let users = response.data.data;
-//   console.log(users)
-  let output = "";
+  // console.log(users)
+  let userList = "";
   users.forEach(function (user) {
-    output += `
+    userList += `
       <li>
       <a href="#" onclick="return myFunction();">
           <div class="d-flex">                            
               <div class="chat-user-img online align-self-center me-3 ms-0">
-                  <img src="assets/images/users/avatar-3.jpg" class="rounded-circle avatar-xs" alt="">
+                  <img src="assets/images/users/user.png" class="rounded-circle avatar-xs" alt="">
               </div>
 
               <div class="flex-1 overflow-hidden">
@@ -21,12 +21,65 @@ axios.get("https://belltro.xyz:129/numbers").then(function (response) {
       </a>
   </li>
   `;
-
   });
-  document.getElementById("output").innerHTML = output;
-  // console.log(response.status); // ex.: 200
+  document.querySelector("#userList").innerHTML = userList;
 });
 
+function myFunction() {
+  const userNumber = event.target.innerText;
+   //  console.log(userNumber)
+ 
+  axios.get(`https://belltro.xyz:129/conversations?number=whatsapp:${userNumber}`).then(function (response) {
+  let messages = response.data.data;
+  //   console.log(messages);
+  let conversationList = "";
+  messages.forEach(function (message) {
+  conversationList += `
+  <li id="message">
+  <div class="conversation-list">
+  <div class="chat-avatar">
+  <img src="assets/images/users/user.png" alt="">
+  </div>
+  
+  <div class="user-chat-content">
+  <div class="ctext-wrap">
+  <div class="ctext-wrap-content">
+  <p class="mb-0">
+  ${message.message }
+  </p>
+  </div>
+  </div>
+  </div>
+  </div>
+  </li> 
+  
+  <li class="right" id="last_message">
+  <div class="conversation-list">
+  <div class="chat-avatar">
+  <img src="assets/images/users/tuji_avatar.png" alt="">
+  </div>
+  
+  <div class="user-chat-content">
+  <div class="ctext-wrap">
+  <div class="ctext-wrap-content">
+  <p class="mb-0">
+  ${message.last_message.replace(
+      /\n/g,
+      "</br>"
+  )}
+  </p>
+  </div>
+  </div>
+  </div>
+  </div>
+  </li> `;
+  });
+  document.getElementById("conversation-list").innerHTML = conversationList;
+  let avatar = 
+      `<h5 class="font-size-16 mb-0 text-truncate"><a  class="text-reset user-profile-show">${userNumber}</a></h5> `
+          document.getElementById("avatar-number").innerHTML = avatar;
+  });
+                      }
 
 
 
